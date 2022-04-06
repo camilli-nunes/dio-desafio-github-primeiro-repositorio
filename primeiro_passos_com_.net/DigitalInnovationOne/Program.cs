5316d3d04727b3a0cf3d46564f1e1576a5016f19
camilli-nunes <arienne.nunes@hotmail.com>
2022-04-06 14:56:28 -0300

    Acrescentando informações da aula de .NET

---
+++ primeiro_passos_com_.net/DigitalInnovationOne/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroDeVezes = 5;
+for (int i=0; i <= numeroDeVezes; i++)
+Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
